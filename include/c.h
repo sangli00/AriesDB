b6f0dc467c040e3d7ac984ce4e5a34719e496768
root <root@vm10-0-2-20.ksc.com>
2020-05-20 00:32:24 +0800

    create page.h & c.h

---
+++ include/c.h
@@ -0,0 +1,22 @@
+#ifndef __ARIES_C_H__
+#define __ARIES_C_H__
+
+
+#ifndef bool
+typedef unsigned char bool;
+#endif
+
+#ifndef true
+#define true    ((bool) 1)
+#endif
+
+#ifndef false
+#define false   ((bool) 0)
+#endif
+
+
+
+typedef long int int64;
+typedef unsigned long int uint64;
+
+#endif
---
+++ include/storage/page.h
@@ -0,0 +1,11 @@
+#ifndef __ARIES_PAGE_H__
+#define __ARIES_PAGE_H__
+
+#include "c.h"
+
+struct Page{
+	uint64 id; /* page id */
+}Page;
+
+#endif
+
